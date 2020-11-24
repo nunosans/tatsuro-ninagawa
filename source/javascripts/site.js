22a867e55a03c289ff8679418b93bb1dfec6a948
Nuno Coelho Santos <nunocs@google.com>
2020-11-24 23:24:46 +0000

    Add simple language redirect for Japanese

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,9 @@
-// This is where it all goes :)
+$(document).ready(function(){
+
+  // Redirect to Japanese page if browser is in Japanese.
+  /* var userLang = navigator.language || navigator.userLanguage;
+  if (userLang.substring(0, 2) == "ja") {
+    window.location.href = "/ja"
+  } */
+
+});
