5963504efb843b6217aa6dc4beb2a574fad4feb5
Nuno Coelho Santos <nunocs@google.com>
2020-11-25 18:16:02 +0000

    Add loading color

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+  $('body').addClass('ready');
+
   // Redirect to Japanese page if browser is in Japanese.
   /* var userLang = navigator.language || navigator.userLanguage;
   if (userLang.substring(0, 2) == "ja") {
