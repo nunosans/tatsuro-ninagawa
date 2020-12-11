3609c03aac859b2c5e181197d978f174b7256ee5
Nuno Coelho Santos <nuno@nunocoelhosantos.com>
2020-12-11 10:12:03 +0000

    New redirect, placeholder image

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 
   // Redirect to Japanese page if browser is in Japanese.
-  var userLang = navigator.language || navigator.userLanguage;
-  if (userLang.substring(0, 2) == "ja") {
-    window.location.href = "/ja"
-  }
+  // var userLang = navigator.language || navigator.userLanguage;
+  // if (userLang.substring(0, 2) == "ja") {
+  //   window.location.href = "/ja"
+  // }
 
   $('*[data-action="show-bio"').on('click', function(event) {
     event.preventDefault();
